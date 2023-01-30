2053b995ea5abc8842d4bb99a7b4b252ba4959a5
Antonio Aguilar <antonioaja@protonmail.com>
2023-01-30 11:28:12 -0500

    Updated for basic png compression

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use anyhow::*;
 use clap::Parser;
+use image::ImageEncoder;
 use std::fs::File;
 use std::io::Write;
 
-pub mod formats;
-
 #[derive(Parser, Debug)]
-#[clap(author, version, about = "A CLI program to batch convert image files", long_about = None)]
+#[clap(author, version, about = "A CLI program to convert image files", long_about = None)]
 struct Args {
-    /// The file to delete meta from
+    /// The file to compress
     #[clap(short, long, value_parser)]
     file: String,
+
+    /// Output file (include extension)
+    #[clap(short, long, value_parser)]
+    output: String,
+
+    /// The speed of compression (1 => slowest ; 10 => fastest)
+    #[clap(short, long, value_parser, default_value_t = 3)]
+    speed: u8,
 
     /// Determine the quality of lossy re-encoding (maximum of 100)
     #[clap(short, long, value_parser, default_value_t = 80)]
@@ -21,21 +28,31 @@ fn main() -> Result<()> {
     // Collect arguments
     let args: Args = Args::parse();
     let input_file = &args.file;
-    let quality: u8 = args.quality.clamp(1, 100);
+    let output_file = &args.output;
+    let _quality: u8 = args.quality.clamp(1, 100);
+    let _speed = args.speed.clamp(1, 10);
 
-    if formats::ext_is_valid(input_file) {
-        let data = match formats::extension(input_file) {
-            ".png" => formats::png::reencode_png(input_file)?,
-            ".jpg" => formats::jpg::reencode_jpg(input_file, quality)?,
-            ".jpeg" => formats::jpg::reencode_jpg(input_file, quality)?,
-            _ => [].to_vec(),
-        };
+    if ext_is_valid(input_file) {
+        let image = image::open(input_file).context(format!("Could not open {}", input_file))?;
 
-        if data.is_empty() {
-            bail!("File is not supported");
-        }
+        let mut out = File::create(output_file).context("Unable to create file")?;
 
-        let mut out = File::create(input_file).context("Unable to create file")?;
+        let mut data: Vec<u8> = [].to_vec();
+
+        let encoder = image::codecs::png::PngEncoder::new_with_quality(
+            &mut data,
+            image::codecs::png::CompressionType::Best,
+            image::codecs::png::FilterType::Adaptive,
+        );
+
+        encoder
+            .write_image(
+                image.as_bytes(),
+                image.width(),
+                image.height(),
+                image.color(),
+            )
+            .context("Could not encode image")?;
 
         out.write_all(&data).context("Could not write to file")?;
     } else {
@@ -43,4 +60,18 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+pub fn ext_is_valid(filename: &str) -> bool {
+    matches!(
+        extension(filename),
+        ".jpg" | ".jpeg" | ".png" | ".gif" | ".bmp"
+    )
+}
+pub fn extension(filename: &str) -> &str {
+    filename
+        .rfind('.')
+        .map(|idx| &filename[idx..])
+        .filter(|ext| ext.chars().skip(1).all(|c| c.is_ascii_alphanumeric()))
+        .unwrap_or("")
 }
